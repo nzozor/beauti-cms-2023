68192df8c3c1e7c3a6533bf71d49b07f197ba860
Nicolas Zozor <nick.zozor@gmail.com>
2023-07-31 03:27:24 +0100

    update server.ts 1338

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   // url: env("PUBLIC_URL", "http://192.168.1.114:1666"),
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1666),
+  port: env.int('PORT', 1338),
   app: {
     keys: env.array('APP_KEYS'),
   },
