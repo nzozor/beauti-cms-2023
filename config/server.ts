9d58b294cd1fe7c2a16d5c876582376c393ab91e
Nicolas Zozor <nick.zozor@gmail.com>
2023-07-31 03:58:40 +0100

    update server.ts 1339 dup bis final

--- config/server.ts
+++ config/server.ts
@@ -1,5 +1,5 @@
 export default ({ env }) => ({
-  // url: env("PUBLIC_URL", "http://192.168.1.114:1666"),
+  url: env("PUBLIC_URL", `http://167.99.200.71/:${env.int('PORT', 1339)}`),
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1339),
   app: {
