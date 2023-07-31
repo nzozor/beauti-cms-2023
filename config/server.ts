1b65c5e490b1f2d4d9018d8f4103e8d587347d67
Nicolas Zozor <nick.zozor@gmail.com>
2023-07-31 02:48:28 +0100

    change port

--- config/server.ts
+++ config/server.ts
@@ -1,8 +1,8 @@
 export default ({ env }) => ({
-  url: env("PUBLIC_URL", "http://localhost:1338"),
+  url: env("PUBLIC_URL", "http://localhost:1666"),
 
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1338),
+  port: env.int('PORT', 1666),
   app: {
     keys: env.array('APP_KEYS'),
   },
