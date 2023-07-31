799b9a4e2b919cf3c98933d335770c8ea10d6a19
Nicolas Zozor <nick.zozor@gmail.com>
2023-07-31 01:50:30 +0100

    first commit

---
+++ src/admin/app.tsx
@@ -0,0 +1,39 @@
+import ckeditor5Dll from "ckeditor5/build/ckeditor5-dll.js";
+import ckeditor5MrkdownDll from "@ckeditor/ckeditor5-markdown-gfm/build/markdown-gfm.js";
+
+
+export default {
+  config: {
+    locales: [
+      // 'ar',
+      // 'fr',
+      // 'cs',
+      // 'de',
+      // 'dk',
+      // 'es',
+      // 'he',
+      // 'id',
+      // 'it',
+      // 'ja',
+      // 'ko',
+      // 'ms',
+      // 'nl',
+      // 'no',
+      // 'pl',
+      // 'pt-BR',
+      // 'pt',
+      // 'ru',
+      // 'sk',
+      // 'sv',
+      // 'th',
+      // 'tr',
+      // 'uk',
+      // 'vi',
+      // 'zh-Hans',
+      // 'zh',
+    ],
+  },
+  bootstrap(app) {
+    console.log(app);
+  },
+};
---
+++ src/api/consultation-page/controllers/consultation-page.ts
@@ -0,0 +1,7 @@
+/**
+ * consultation-page controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::consultation-page.consultation-page');
