a719c90a0e2047e81a3b52118af262b6b561f167
Nicolas Zozor <nick.zozor@gmail.com>
2023-08-03 12:16:27 +0100

    fix server.ts

--- config/server.ts
+++ config/server.ts
@@ -1,11 +1,8 @@
+
 export default ({ env }) => ({
-  url: env("PUBLIC_URL", `http://localhost:${env.int('PORT', 1339)}`),
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1339),
+  port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
