568f00032fd725019fa770d384ea4f3b2fdf31cc
Nicolas Zozor <nick.zozor@gmail.com>
2023-08-03 12:46:53 +0100

    fix env

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
-
 export default ({ env }) => ({
+  url: env("PUBLIC_URL", `http://167.99.200.71/:${env.int('PORT', 1339)}`),
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 1339),
   app: {
     keys: env.array('APP_KEYS'),
   },
---
+++ src/api/email/email.ts
@@ -0,0 +1,10 @@
+
+export default {
+  routes: [
+    {
+      "method": "POST",
+      "path": "/send-email",
+      "handler": "email.send"
+    }
+  ]
+}
