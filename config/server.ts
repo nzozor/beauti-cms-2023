74d61958c335ead2ddea31254c71ae6a4c435377
Nicolas Zozor <nick.zozor@gmail.com>
2023-08-03 13:20:05 +0100

    fix env latest

--- config/server.ts
+++ config/server.ts
@@ -1,5 +1,4 @@
 export default ({ env }) => ({
-  url: env("PUBLIC_URL", `http://167.99.200.71/:${env.int('PORT', 1339)}`),
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1339),
   app: {
