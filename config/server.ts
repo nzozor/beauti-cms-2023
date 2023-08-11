579ba06655ada2776552f77184822413d845753e
Nicolas Zozor <nick.zozor@gmail.com>
2023-08-11 17:33:43 +0100

    update db images, .env image full path

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1339),
+  url: env('PUBLIC_URL', 'http://localhost:1339'),
   app: {
     keys: env.array('APP_KEYS'),
   },
