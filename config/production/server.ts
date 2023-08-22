e295bfd81687eb41e66df05f01a0e4840973c704
Nicolas Zozor <nick.zozor@gmail.com>
2023-08-22 21:26:37 +0100

    revert to original

--- config/production/server.ts
+++
@@ -1,9 +0,0 @@
-export default ({ env }) => ({
-  host: env('HOST', '167.99.200.71'),
-  port: env.int('PORT', 1339),
-  url: env('PUBLIC_URL', 'http://167.99.200.71:1339'),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-});
-``
--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,6 @@
 export default ({ env }) => ({
-  host: env('DATABASE_HOST', '127.0.0.1'),
+  host: env('DATABASE_HOST', '0.0.0.0'),
   port: env.int('PORT', 1339),
-  url: env('PUBLIC_URL', 'http://localhost:1339'),
   app: {
     keys: env.array('APP_KEYS'),
   },
