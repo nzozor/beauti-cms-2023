602052b4dd07e6ad522fc29d9b1e60a35c6f19d0
Nicolas Zozor <nick.zozor@gmail.com>
2023-09-01 07:03:54 +0100

    Test public url

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,8 @@
 export default ({ env }) => ({
   host: env('DATABASE_HOST', '0.0.0.0'),
   port: env.int('PORT', 1339),
+  url: env('PUBLIC_URL', 'http://167.99.200.71:1339/'), // THIS ONE
+
   app: {
     keys: env.array('APP_KEYS'),
   },
