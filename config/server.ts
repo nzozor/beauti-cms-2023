9be6e64b4f65ff4493bd3a207f74396537d11956
Nicolas Zozor <nzozor@gmail.com>
2023-09-22 09:20:55 +0000

    add all production new changes september 2023

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('DATABASE_HOST', '0.0.0.0'),
   port: env.int('PORT', 1339),
-  url: env('PUBLIC_URL', 'http://167.99.200.71:1339/'), // THIS ONE
+  url: env('PUBLIC_URL', '0.0.0.0'), // THIS ONE
 
   app: {
     keys: env.array('APP_KEYS'),
